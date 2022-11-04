504c30b1d363f3164b7fdd388d953484644b5f03
Barry McCullough <barrymac20@gmail.com>
2022-11-04 13:36:49 +0000

    Add game() function to play 5 rounds. Tidy up code. Game working in console

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # rock-paper-scissors
 
 This project is going to mimic the popular "Rock, Paper, Scissors" game using html and javascript. Initially this will be played in the console and then at a later stage a user interface will be introduced.
+
+Tasks:
+- Start new GIT repo - DONE
+- Create blank HTML and link to external script.js file - DONE
+- Create getComputerChoice() function to randomly pick an answer for computer - DONE
+- Write a playRound() function that takes two parameters and returns string declaring winner of round - DONE
+- Write a game() function that will play five rounds, keep score and reports winner or loser at end - DONE
--- script.js
+++ script.js
@@ -1,94 +1,58 @@
-// console.log("Hello World");
+const possibleAnswers = ["ROCK", "PAPER", "SCISSORS"] //Create variable for array of answers
+let playerScore = 0
+let computerScore =0
 
-//Create variable for array of answers
-const possibleAnswers = ["ROCK", "PAPER", "SCISSORS"]
 
-
-//Create function to generate random answer
+//Get random answer
 function getComputerChoice() {
-
-//Create variable for random selection of answer
-const randomAnswer = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
-
-return randomAnswer;
-
+  const randomAnswer = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
+  return randomAnswer;
 }
 
-//Create function to play one round of game
+//Play one round
 function playRound(playerSelection, computerSelection) {
 
   playerSelection = prompt("Rock, Paper, or Scissors").toUpperCase();
   computerSelection = getComputerChoice();
 
-  console.log("You Chose: " + playerSelection);
-  console.log("Computer Chose: " + computerSelection);
-  console.log("Score:")
-  console.log("You --> 1")
-  console.log("Computer --> 1")
-  console.log()
-
-//If else statements to cover each selection option
   if (
-    playerSelection === "ROCK" && computerSelection === "SCISSORS"
-  ) {
-    return "You Win! Rock beats Scissors"
-    // console.log("You Win! Rock beats Scissors")
-  } else if (
-    playerSelection === "ROCK" && computerSelection === "PAPER"
-  ) {
-    return "You Lose! Paper beats Rock"
-    // console.log("You Lose! Paper beats Rock")
-  } else if (
-    playerSelection === "SCISSORS" && computerSelection === "ROCK"
-  ) {
-    return "You Lose! Rock beats Scissors"
-    // console.log("You Lose! Rock beats Scissors")
-  } else if (
-    playerSelection === "SCISSORS" && computerSelection === "PAPER"
-  ) {
-    return "You Win! Scissors beats Paper"
-    // console.log("You Win! Scissors beats Paper")
-  }  else if (
+    playerSelection === "ROCK" && computerSelection === "SCISSORS" || 
+    playerSelection === "SCISSORS" && computerSelection === "PAPER" ||
     playerSelection === "PAPER" && computerSelection === "ROCK"
   ) {
-    return "You Win! Paper beats Rock"
-    // console.log("You Win! Paper beats Rock")
+    roundResult = "Player Wins";
+    playerScore++;
   } else if (
+    playerSelection === "ROCK" && computerSelection === "PAPER" || 
+    playerSelection === "SCISSORS" && computerSelection === "ROCK" ||
     playerSelection === "PAPER" && computerSelection === "SCISSORS"
   ) {
-    return "You Lose! Scissors beats Paper"
-    // console.log("You Lose! Scissors beats Paper")
+    roundResult = "Computer Wins";
+    computerScore++;
   } else {
-    return "It's a draw"
-    // console.log("It's a draw")
+    roundResult = "Draw";
+  }
+  return roundResult;
   }
 
-
-}
-// playRound()
-
+//Play multiple rounds
 function game() {
+  playerScore=0;
+  computerScore=0
 
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 5; i++) {
     playRound();
- }
+    console.log(`Round ${i+1} Score:`);
+    console.log(`Player --> ${playerScore} `);
+    console.log(`Computer --> ${computerScore}`);
+  }
+
+  if (playerScore>computerScore) {
+    matchResult = "Player Wins";
+  } else if (playerScore<computerScore) {
+    matchResult = "Computer Wins";
+  } else {
+    matchResult = "Draw Match";
+  }
+  return matchResult;
 }
-
-game()
-
-// playRound(prompt(), prompt())
-
-
-
-// let playerSelection = prompt("Pick Rock, Paper, or Scissors").toUpperCase
-
-//Call play round
-// playRound(prompt("Pick Rock, Paper, or Scissors").toUpperCase(), "SCISSORS")
-
-// getComputerChoice();
-
-
-
-
-
-
