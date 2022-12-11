d7b269cc212c4b2de4f5b09a8c0f2da621e9a5d7
Barry McCullough <barrymac20@gmail.com>
2022-12-11 15:22:29 +0000

    Add buttons, event listeners, divs, running score, winner at 5 points

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-//Create variable for array of answers
+//Variables
 const possibleAnswers = ["ROCK", "PAPER", "SCISSORS"]; 
-
+const buttons = document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', playRound)
+})
+const player = document.getElementById('player-score');
+const computer = document.getElementById('computer-score');
+const matchResult = document.getElementById('match-result');
 let playerScore = 0;
-let computerScore =0;
-
+let computerScore = 0;
 
 //Get random answer
 function getComputerChoice() {
   const randomAnswer = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
   return randomAnswer;
+  //console.log(randomAnswer)
 }
 
 //Play one round
-function playRound(playerSelection, computerSelection) {
+function playRound(e, computerSelection) {
 
-  playerSelection = prompt("Rock, Paper, or Scissors").toUpperCase();
-  computerSelection = getComputerChoice();
+  playerSelection = e.target.value.toLowerCase();
+  // console.log(playerSelection);
+  computerSelection = getComputerChoice().toLowerCase();
+  // console.log(computerSelection);
+
+  if (playerScore < 5 && computerScore < 5) {
 
   if (playerSelection === computerSelection) {
     roundResult = "Draw";
   }
   else if (
-    playerSelection === "ROCK" && computerSelection === "SCISSORS" || 
-    playerSelection === "SCISSORS" && computerSelection === "PAPER" ||
-    playerSelection === "PAPER" && computerSelection === "ROCK"
+    playerSelection === "rock" && computerSelection === "scissors" || 
+    playerSelection === "scissors" && computerSelection === "paper" ||
+    playerSelection === "paper" && computerSelection === "rock"
   ) {
     roundResult = "Player Wins";
     playerScore++;
@@ -31,30 +40,40 @@ function playRound(playerSelection, computerSelection) {
     roundResult = "Computer Wins";
     computerScore++;
   }
-  return roundResult;
+  player.textContent = `Player Score: ${playerScore}`
+  computer.textContent = `Computer Score: ${computerScore}`;
+  // return roundResult;
+  // console.log(playerScore)
+  // } while (playerScore < 5)
+} else {
+  if (playerScore === 5) {
+  matchResult.textContent = 'You win'
+  } else {
+    matchResult.textContent = 'Computer Wins'
   }
+}
+}
+  
 
 //Play multiple rounds
-function game() {
-  playerScore=0;
-  computerScore=0
+// function game() {
+//   playerScore=0;
+//   computerScore=0
 
-  for (let i = 0; i < 5; i++) {
-    playRound();
-    console.log(`Round ${i+1} Score:`);
-    console.log(`Player --> ${playerScore} `);
-    console.log(`Computer --> ${computerScore}`);
-  }
+//   for (let i = 0; i < 5; i++) {
+//     playRound();
+//     console.log(`Round ${i+1} Score:`);
+//     console.log(`Player --> ${playerScore} `);
+//     console.log(`Computer --> ${computerScore}`);
+//   }
 
-  if (playerScore>computerScore) {
-    matchResult = "Player Wins";
-  } else if (playerScore<computerScore) {
-    matchResult = "Computer Wins";
-  } else {
-    matchResult = "Draw Match";
-  }
-  // return matchResult;
-  console.log(matchResult)
-}
-
-game()
+//   if (playerScore>computerScore) {
+//     matchResult = "Player Wins";
+//   } else if (playerScore<computerScore) {
+//     matchResult = "Computer Wins";
+//   } else {
+//     matchResult = "Draw Match";
+//   }
+//   // return matchResult;
+//   console.log(matchResult)
+// }
