66530fbf15b29de5b75fd85b3196caf20c166a6e
Barry McCullough <barrymac20@gmail.com>
2022-12-12 16:35:23 +0000

    Fully functioning logic, very basic UI

--- script.js
+++ script.js
@@ -1,79 +1,63 @@
-//Variables
+// Variables
 const possibleAnswers = ["ROCK", "PAPER", "SCISSORS"]; 
 const buttons = document.querySelectorAll('button').forEach(button => {
   button.addEventListener('click', playRound)
 })
+const playerPick = document.getElementById('player-pick');
+const computerPick = document.getElementById('computer-pick');
+const roundResult = document.getElementById('round-result');
 const player = document.getElementById('player-score');
 const computer = document.getElementById('computer-score');
 const matchResult = document.getElementById('match-result');
 let playerScore = 0;
 let computerScore = 0;
 
-//Get random answer
+// Get random answer for computer choice
 function getComputerChoice() {
   const randomAnswer = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
   return randomAnswer;
-  //console.log(randomAnswer)
 }
 
-//Play one round
+// Play Round
 function playRound(e, computerSelection) {
 
-  playerSelection = e.target.value.toLowerCase();
-  // console.log(playerSelection);
-  computerSelection = getComputerChoice().toLowerCase();
-  // console.log(computerSelection);
+  playerSelection = e.target.value.toUpperCase();
+  computerSelection = getComputerChoice().toUpperCase();
 
   if (playerScore < 5 && computerScore < 5) {
-
-  if (playerSelection === computerSelection) {
-    roundResult = "Draw";
+      
+    if (playerSelection === computerSelection) {
+      roundWinner = "Draw";
+      }
+      else if (
+        playerSelection === "rock" && computerSelection === "scissors" || 
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "rock"
+      ) {
+        roundWinner = `You Win - ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+      } else {
+        roundWinner = `Computer Wins - ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+      }
+      playerPick.textContent = `You picked: ${playerSelection}`
+      computerPick.textContent = `Computer picked: ${computerSelection}`
+      roundResult.textContent = `Round Result: ${roundWinner}`
+      player.textContent = `Your Score: ${playerScore}`
+      computer.textContent = `Computer Score: ${computerScore}`;
+  } 
+    
+  if (playerScore === 5 || computerScore === 5) {
+      announceWinner();
   }
-  else if (
-    playerSelection === "rock" && computerSelection === "scissors" || 
-    playerSelection === "scissors" && computerSelection === "paper" ||
-    playerSelection === "paper" && computerSelection === "rock"
-  ) {
-    roundResult = "Player Wins";
-    playerScore++;
-  } else {
-    roundResult = "Computer Wins";
-    computerScore++;
-  }
-  player.textContent = `Player Score: ${playerScore}`
-  computer.textContent = `Computer Score: ${computerScore}`;
-  // return roundResult;
-  // console.log(playerScore)
-  // } while (playerScore < 5)
-} else {
-  if (playerScore === 5) {
-  matchResult.textContent = 'You win'
-  } else {
-    matchResult.textContent = 'Computer Wins'
-  }
-}
-}
   
+}
 
-//Play multiple rounds
-// function game() {
-//   playerScore=0;
-//   computerScore=0
-
-//   for (let i = 0; i < 5; i++) {
-//     playRound();
-//     console.log(`Round ${i+1} Score:`);
-//     console.log(`Player --> ${playerScore} `);
-//     console.log(`Computer --> ${computerScore}`);
-//   }
-
-//   if (playerScore>computerScore) {
-//     matchResult = "Player Wins";
-//   } else if (playerScore<computerScore) {
-//     matchResult = "Computer Wins";
-//   } else {
-//     matchResult = "Draw Match";
-//   }
-//   // return matchResult;
-//   console.log(matchResult)
-// }
+// Announce winner
+function announceWinner() {
+    if (playerScore === 5) {
+    matchResult.textContent = 'You win the game'
+    } else {
+      matchResult.textContent = 'Computer Wins the game'
+    }
+}
